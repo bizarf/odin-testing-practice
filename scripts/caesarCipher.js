28723805e7682bb3e1975e3f38f7e4560a378a9f
bizarf <snesmaster40@googlemail.com>
2022-11-19 18:05:52 +0000

    Four out of five scripts with tests completed

--- scripts/caesarCipher.js
+++ scripts/caesarCipher.js
@@ -1,5 +1,47 @@
-const caesarCipher = (string) => {
+const caesarCipher = (string, shift) => {
+    if (typeof shift != 'number') return 'Error. The provided shift value is not an integer.'
+    if (shift > 25) return 'Shift value is too high. The maximum accepted value is 25.'
 
+    // split up the string, and then loop through each letter
+    const letters = string.split('');
+    for (let i = 0; i < letters.length; i++) {
+        // this gets the unicode decimal value of the letter
+        const unicode = letters[i].charCodeAt();
+
+        let remainingShift
+
+        // if between A-Y then shift the unicode
+        if (unicode >= 65 && unicode < 90) {
+            // makes sure letters wrap from A-Y
+            if ((unicode + shift) > 90) {
+                remainingShift = (unicode + shift) - 90;
+                letters[i] = String.fromCharCode(64 + remainingShift);
+            } else {
+                letters[i] = String.fromCharCode(unicode + shift);
+            }
+        };
+
+        // if between a-y then shift the unicode
+        if (unicode >= 97 && unicode < 122) {
+            if ((unicode + shift) > 122) {
+                remainingShift = (unicode + shift) - 122;
+                letters[i] = String.fromCharCode(96 + remainingShift);
+            } else {
+                letters[i] = String.fromCharCode(unicode + shift)
+            }
+        }
+
+        // if unicode is Z then set the letter to A
+        if (unicode === 90) {
+            letters[i] = String.fromCharCode(65);
+        };
+
+        // if unicode is z then set the letter to a
+        if (unicode === 122) {
+            letters[i] = String.fromCharCode(97);
+        }
+    };
+    return letters.join('')
 };
 
 module.exports = caesarCipher;
--- tests/caesarCipher.test.js
+++ tests/caesarCipher.test.js
@@ -1,5 +1,29 @@
 const caesarCipher = require('../scripts/caesarCipher');
 
-test('', () => {
-    expect(caesarCipher('')).toBe('');
+test(`takes the string 'this is a test', shifts the characters by 1, and returns 'uijt jt b uftu'`, () => {
+    expect(caesarCipher('this is a test', 1)).toBe('uijt jt b uftu');
+});
+
+test(`return an error message if the shift value is not an integer`, () => {
+    expect(caesarCipher('this is a test', '2')).toBe('Error. The provided shift value is not an integer.');
+});
+
+test(`return an error message if the shift value is beyond 25`, () => {
+    expect(caesarCipher('this is a test', 30)).toBe('Shift value is too high. The maximum accepted value is 25.');
+});
+
+test(`takes the string 'this is a test', shifts the characters by 5, and returns 'ymnx nx f yjxy'`, () => {
+    expect(caesarCipher('this is a test', 5)).toBe('ymnx nx f yjxy');
+});
+
+test(`takes the string 'this is a test', shifts the characters by 25, and returns 'sghr hr z sdrs'`, () => {
+    expect(caesarCipher('this is a test', 25)).toBe('sghr hr z sdrs');
+});
+
+test(`takes the string 'Javascript is a lot of fun!', shifts the characters by 5, and returns 'Ofafxhwnuy nx f qty tk kzs!'`, () => {
+    expect(caesarCipher('Javascript is a lot of fun!', 5)).toBe('Ofafxhwnuy nx f qty tk kzs!');
+});
+
+test(`takes the string 'ThIs A sUpEr TeSt!#', shifts the characters by 15, and returns 'IwXh P hJeTg ItHi!#'`, () => {
+    expect(caesarCipher('ThIs A sUpEr TeSt!#', 15)).toBe('IwXh P hJeTg ItHi!#');
 });
